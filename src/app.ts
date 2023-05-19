65565fb8c9da28bbab214fecbeabe8d1adccf3c0
haena-j <user@userui-MacBookPro.local>
2023-05-19 23:42:00 +0900

    feat: Add explicit type annotations in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import swaggerUi from 'swagger-ui-express';
+import express, { Application, Request, Response } from 'express';
+import swaggerUi, { SwaggerOptions } from 'swagger-ui-express';
 import fs from 'fs';
 import YAML from 'yamljs';
 import path from 'path';
 
-const app = express();
-const port = 3000;
+const app: Application = express();
+const port: number = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
 // Swagger 설정
 if (fs.existsSync(path.join(__dirname, "swagger.yaml"))) {
-  const swaggerSpec = YAML.load(path.join(__dirname, "swagger.yaml"));
+  const swaggerSpec: SwaggerOptions = YAML.load(path.join(__dirname, "swagger.yaml"));
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
